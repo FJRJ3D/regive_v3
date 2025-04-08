49590fede1c0c608120f6f865e904d993014c238
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-08 10:52:36 +0200

    feat(admin): begin development of manage products page

--- lib/screens/ProductManagerScreen.dart
+++ lib/screens/ProductManagerScreen.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class ProductManagerScreen extends ConsumerWidget {
+class ProductManagerScreen extends ConsumerStatefulWidget {
   const ProductManagerScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ProductManagerScreen> createState() =>
+      _ProductManagerScreenState();
+}
+
+class _ProductManagerScreenState extends ConsumerState<ProductManagerScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Product Manager Screen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text('Ir a otra pantalla'),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFEFA354), Color(0xFFE56230)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              SvgPicture.asset('assets/ReGive_logo_horizontal.svg', width: 180),
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFE0C0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: 700,
+                width: double.infinity,
+              ),
+            ],
+          ),
         ),
       ),
     );
