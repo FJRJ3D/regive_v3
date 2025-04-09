9fa44dca902f27d13804ae8bd211c4fe82000146
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-09 09:17:35 +0200

    refactor: update RegisterScreen to include new provider

--- lib/navigators/AppRouter.dart
+++ lib/navigators/AppRouter.dart
@@ -3,8 +3,6 @@ import 'package:regive_v3/navigators/TabBarWidget.dart';
 import 'package:regive_v3/screens/LoginScreen.dart';
 import 'package:regive_v3/screens/RegisterScreen.dart';
 
-
-
 class AppNavigator {
   late final GoRouter _router;
 
@@ -12,16 +10,10 @@ class AppNavigator {
     _router = GoRouter(
       initialLocation: '/',
       routes: [
-        // GoRoute(
-        //   path: '/',
-        //   builder: (context, state) => const LoginScreen(),
-        // ),
-        //=========================================================Poner bien, solo para pruebas
         GoRoute(
           path: '/',
-          builder: (context, state) => const TabBarWidget(),
+          builder: (context, state) => const LoginScreen(),
         ),
-        //=========================================================Poner bien, solo para pruebas
         GoRoute(path: '/main', builder: (context, state) => const TabBarWidget()),
         GoRoute(path: '/register', builder: (context, state) => const RegisterScreen()),
       ],
--- lib/screens/RegisterScreen.dart
+++ lib/screens/RegisterScreen.dart
@@ -6,46 +6,15 @@ import 'package:regive_v3/components/CustomTextField.dart';
 import 'package:regive_v3/navigators/AppRouter.dart';
 import 'package:regive_v3/repositories/AuthRepository.dart';
 
-class RegisterScreen extends ConsumerStatefulWidget {
+class RegisterScreen extends ConsumerWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<RegisterScreen> createState() =>
-      _RegisterScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _displayNameController = TextEditingController();
 
-class _RegisterScreenState extends ConsumerState<RegisterScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _displayNameController = TextEditingController();
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _displayNameController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _register() async {
-    final authRepository = ref.read(authRepositoryProvider);
-    final userCredential = await authRepository.registerWithEmailPassword(
-      email: _emailController.text,
-      password: _passwordController.text,
-      displayName: _displayNameController.text,
-    );
-
-    if (userCredential != null) {
-      AppNavigator().router.go('/');
-    } else {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('Password should be at least 6 characters')));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
@@ -95,7 +64,22 @@ class _RegisterScreenState extends ConsumerState<RegisterScreen> {
                       CustomElevatedButton(
                         text: 'Enter',
                         onPressed: () async {
-                          await _register();
+                          final userCredential = await ref.watch(
+                            registerWithEmailPasswordProvider(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                              displayName: _displayNameController.text,
+                            ).future,
+                          );
+                          if (userCredential != null) {
+                            AppNavigator().router.go('/');
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Password should be at least 6 characters'),
+                              ),
+                            );
+                          }
                         },
                         backgroundColor: const Color(0xFFE66A35),
                         foregroundColor: Colors.white,
