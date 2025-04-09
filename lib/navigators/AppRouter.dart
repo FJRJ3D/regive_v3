15edfe484b269dc39c15ccc609ba1070356b3604
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-09 18:39:43 +0200

    Temporarily modified GoRouter to test other pages

--- lib/navigators/AppRouter.dart
+++ lib/navigators/AppRouter.dart
@@ -10,10 +10,16 @@ class AppNavigator {
     _router = GoRouter(
       initialLocation: '/',
       routes: [
+        // GoRoute(
+        //   path: '/',
+        //   builder: (context, state) => const LoginScreen(),
+        // ),
+        //=========================================================Poner bien, solo para pruebas
         GoRoute(
           path: '/',
-          builder: (context, state) => const LoginScreen(),
+          builder: (context, state) => const TabBarWidget(),
         ),
+        //=========================================================Poner bien, solo para pruebas
         GoRoute(path: '/main', builder: (context, state) => const TabBarWidget()),
         GoRoute(path: '/register', builder: (context, state) => const RegisterScreen()),
       ],
