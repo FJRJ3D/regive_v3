d8ad74ab0bf2e2c1582ff47114dd7e37340a6b03
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-09 18:00:24 +0200

    Updated ProductManagerScreen with UI modifications and improvements

--- lib/screens/ProductManagerScreen.dart
+++ lib/screens/ProductManagerScreen.dart
@@ -1,25 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:regive_v3/components/CustomTextField.dart';
 
 class ProductManagerScreen extends ConsumerWidget {
   const ProductManagerScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Align(alignment: Alignment.topLeft,child: SizedBox(
-      height: 50, // Ajustamos la altura
-      width: 200, // Aumentamos el ancho
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue, // Establecemos el color azul
-          elevation: 10, // Añadimos una sombra
-          shadowColor: Colors.black, // Puedes cambiar el color de la sombra
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(
+              labelText: 'Search a product',
+              hintText: 'Enter a product name',
+              prefixIcon: Icons.search,
+              keyboardType: TextInputType.text,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(100),
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                  color: Colors.white,
+                ),
+                width: double.infinity,
+                height: 140,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 185,
+                      height: double.infinity,
+                      child: Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Huismus%2C_man.jpg/250px-Huismus%2C_man.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ],
         ),
-        child: Text('Ir a otra pantalla'),
       ),
-    )
-
     );
   }
 }
