b2714e10f275158c197f63c80cff91ac6e05b1fd
Maksym7576 <ukrainemaksymhryshchuk@gmail.com>
2025-04-11 12:00:21 +0200

    add owner component and function dateFormat

--- lib/components/OwnerComponent.dart
+++ lib/components/OwnerComponent.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:regive_v3/providers/global_providers.dart';
 import 'package:regive_v3/repositories/user_details_repository.dart';
-
+import 'package:regive_v3/utils/date_format.dart';
 class OwnerComponent extends ConsumerWidget {
   const OwnerComponent({super.key});
 
@@ -20,28 +20,68 @@ class OwnerComponent extends ConsumerWidget {
         if (userDetails == null) {
           return const Center(child: Text('No user details found'));
         }
-        return Container(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage:
-                        userDetails.imageUrl != null
-                            ? NetworkImage(userDetails!.imageUrl!)
-                            : AssetImage('assets/images/default_user_photo.png')
-                                as ImageProvider,
+        final userWasLastTime = ref.watch(formatDateProvider(ref ,userDetails.wasOnline.toDate()));
+        return Card(
+          elevation: 2,
+          margin: const EdgeInsets.all(12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.amberAccent, width: 1.5),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 8,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  Text(
-                    userDetails.username,
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
-                  )
-                ],
-              ),
-            ],
+                  child: CircleAvatar(
+                    radius: 36,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: userDetails.imageUrl != null
+                    ? NetworkImage(userDetails.imageUrl!)
+                    : const AssetImage('assets/images/default_user_photo.png')
+                    as ImageProvider,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        userDetails.username,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Text(
+                            "was online ${userWasLastTime}",
+                            style: const TextStyle(
+                              fontSize: 11,
+                              color: Colors.black26,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
--- lib/models/UserDetails.dart
+++ lib/models/UserDetails.dart
@@ -4,11 +4,13 @@ class UserDetails {
   final String id;
   final String username;
   final String imageUrl;
+  final Timestamp wasOnline;
 
   UserDetails({
     required this.id,
     required this.username,
     required this.imageUrl,
+    required this.wasOnline,
 });
 
   Map<String, dynamic> toMap() {
@@ -16,11 +18,12 @@ class UserDetails {
       'id': id,
       'name': username,
       'imageUrl': imageUrl,
+      'wasOnline': wasOnline,
     };
   }
 
   factory UserDetails.formDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc) {
     final data = doc.data()!;
-    return UserDetails(id: doc.id, username: data['username'], imageUrl: data['imageUrl']);
+    return UserDetails(id: doc.id, username: data['username'], imageUrl: data['imageUrl'], wasOnline: data['wasOnline']);
   }
 }
