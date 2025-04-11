b6f47afa0c7cb19427374210dc8485fc7511e175
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-11 11:08:13 +0200

    Retrieve products from Firestore and create ProductCard component

--- lib/screens/ProductManagerScreen.dart
+++ lib/screens/ProductManagerScreen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:regive_v3/components/CustomTextField.dart';
+import 'package:regive_v3/components/ProductCard.dart';
+import 'package:regive_v3/repositories/product_repository.dart';
 
 class ProductManagerScreen extends ConsumerWidget {
   const ProductManagerScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final userProductsAsync = ref.watch(getAllUserProductsProvider);
+
     return Align(
       alignment: Alignment.topCenter,
       child: SingleChildScrollView(
@@ -18,138 +22,20 @@ class ProductManagerScreen extends ConsumerWidget {
               prefixIcon: Icons.search,
               keyboardType: TextInputType.text,
             ),
-            Material(
-              color: Colors.transparent,
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(100),
-                onTap: () {
-                  print("boton presionado");
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withAlpha(100),
-                          blurRadius: 10,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                      color: Colors.white,
-                    ),
-                    width: double.infinity,
-                    height: 140,
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return Row(
-                          children: [
-                            Stack(
-                              children: [
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.43,
-                                  height: double.infinity,
-                                  child: Image.network(
-                                    'https://cdn.wallapop.com/images/10420/ij/if/__/c10420p1121163805/i5556199917.jpg?pictureSize=W640',
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: constraints.maxWidth * 0.43,
-                                  height: double.infinity,
-                                  child: Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Container(
-                                      alignment: Alignment.center,
-                                      height: 25,
-                                      width: 120,
-                                      decoration: BoxDecoration(
-                                        color: Colors.black.withAlpha(150),
-                                        borderRadius: BorderRadius.only(
-                                          bottomLeft: Radius.circular(5),
-                                          topRight: Radius.circular(5),
-                                        ),
-                                      ),
-                                      child: Text(
-                                        "Ropa de niño 10 años",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                        ),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Flexible(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(5),
-                                      child: Align(
-                                        alignment: Alignment.topLeft,
-                                        child: Text(
-                                          "25/03/2025",
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                            fontSize: 12,
-                                            color: Color(0xFF8D8D8D),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Flexible(
-                                    flex: 2,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(5),
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(
-                                          "Lote compuesto por  - chándal, pantalón con bolsillos y sudadera de cremallera ( la parte de delante tiene un mínimo...",
-                                          maxLines: 3,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                            color: Color(0xFF8D8D8D),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Flexible(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(5),
-                                      child: Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Text(
-                                          "Orders: 5",
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                            color: Color(0xFF8D8D8D),
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
+            userProductsAsync.when(
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (error, _) => Center(child: Text('Error: $error')),
+              data: (products) {
+                if (products.isEmpty) {
+                  return const Center(child: Text("No products found."));
+                }
+                return Column(
+                  children:
+                      products
+                          .map((product) => ProductCard(product: product))
+                          .toList(),
+                );
+              },
             ),
           ],
         ),
