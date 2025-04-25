f56f7291f13b8b182d82c729fdfa0c6faf3bf0de
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-04-25 11:18:45 +0200

    Added shadow properly to order card

--- lib/components/OrderCard.dart
+++ lib/components/OrderCard.dart
@@ -10,12 +10,8 @@ class OrderCard extends ConsumerWidget {
   final String? status;
   final String? orderId;
 
-  const OrderCard({
-    Key? key,
-    required this.product,
-    this.status,
-    this.orderId,
-  }) : super(key: key);
+  const OrderCard({Key? key, required this.product, this.status, this.orderId})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -23,161 +19,111 @@ class OrderCard extends ConsumerWidget {
       padding:
           const EdgeInsets.only(bottom: 10) +
           EdgeInsets.symmetric(horizontal: 15),
-      child: Stack(
-        children: [
-          Column(
-            // mainAxisSize: valor,
-            children: [
-              Container(
-                height: 75,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.transparent,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withAlpha(100),
-                      blurRadius: 10,
-                      offset: Offset(0, 4),
-                    ),
-                  ],
-                ),
+      child: Material(
+        color: Colors.transparent,
+        elevation: 7,
+        shadowColor: Colors.black.withAlpha(500),
+        borderRadius: BorderRadius.circular(40),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(40),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Colors.white.withAlpha(170),
               ),
-            ],
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(40),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.white.withAlpha(170),
-                ),
-                width: double.infinity,
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(100),
-                    onTap: () {},
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 6,
-                            vertical: 4,
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Row(
-                                children: [
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: ClipOval(
-                                      child: SizedBox(
-                                        width: 67,
-                                        height: 67,
-                                        child: Image.network(
-                                          product.imageUrl,
-                                          fit: BoxFit.cover,
-                                        ),
+              width: double.infinity,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(100),
+                  onTap: () {},
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 6,
+                          vertical: 4,
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              children: [
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: ClipOval(
+                                    child: SizedBox(
+                                      width: 67,
+                                      height: 67,
+                                      child: Image.network(
+                                        product.imageUrl,
+                                        fit: BoxFit.cover,
                                       ),
                                     ),
                                   ),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Container(
-                                      alignment: Alignment.center,
-                                      height: 25,
-                                      width: 120,
-                                      decoration: BoxDecoration(
-                                        color: Colors.black.withAlpha(150),
-                                        borderRadius: BorderRadius.only(
-                                          bottomLeft: Radius.circular(5),
-                                          topRight: Radius.circular(5),
-                                        ),
-                                      ),
-                                      child: Text(
-                                        product.name,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                        ),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    height: 25,
+                                    width: 120,
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withAlpha(150),
+                                      borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(5),
+                                        topRight: Radius.circular(5),
                                       ),
                                     ),
+                                    child: Text(
+                                      product.name,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
                                   ),
-                                  Expanded(
-                                    child: Flexible(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(5),
-                                        child: Align(
-                                          alignment: Alignment.centerRight,
-                                          child: Text(
-                                            DateFormat(
-                                              'yyyy-MM-dd HH:mm',
-                                            ).format(
-                                              product.publishedDate.toDate(),
-                                            ),
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 11,
-                                              color: Color(0xFF8D8D8D),
-                                            ),
+                                ),
+                                Expanded(
+                                  child: Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(5),
+                                      child: Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Text(
+                                          DateFormat('yyyy-MM-dd HH:mm').format(
+                                            product.publishedDate.toDate(),
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 11,
+                                            color: Color(0xFF8D8D8D),
                                           ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                ],
-                              ),
-
-                              // TextFormField(
-                              //   decoration: InputDecoration(
-                              //     labelText: 'Enter your text',
-                              //     hintText: 'This is a hint',
-                              //     border: OutlineInputBorder(
-                              //       borderRadius: BorderRadius.circular(12),
-                              //     ),
-                              //     focusedBorder: OutlineInputBorder(
-                              //       borderRadius: BorderRadius.circular(12),
-                              //       borderSide: BorderSide(color: Colors.blue, width: 2),
-                              //     ),
-                              //     enabledBorder: OutlineInputBorder(
-                              //       borderRadius: BorderRadius.circular(12),
-                              //       borderSide: BorderSide(color: Colors.grey, width: 1),
-                              //     ),
-                              //   ),
-                              //   keyboardType: TextInputType.text,
-                              //   obscureText: false, // Set to true if you want to hide the text (e.g., for passwords)
-                              //   validator: (value) {
-                              //     if (value == null || value.isEmpty) {
-                              //       return 'Please enter some text';
-                              //     }
-                              //     return null;
-                              //   },
-                              //   onSaved: (value) {
-                              //     // Save the value to a variable or perform further actions
-                              //   },
-                              // ),
-
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      )
     );
   }
 }
